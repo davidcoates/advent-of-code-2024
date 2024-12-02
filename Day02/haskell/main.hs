58a505f7cb219c40c1ca4b63fcf7ef8e23229a69
David <davidcoates95@gmail.com>
2024-12-02 16:50:02 +1100

    day2 haskell

---
+++ Day02/haskell/main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE LambdaCase #-}
+
+readReports :: IO [[Int]]
+readReports = do
+  contents <- readFile "../input.txt"
+  return $ map (map read . words) (lines contents)
+
+isReportSafeInc :: [Int] -> Bool
+isReportSafeInc = \case
+  (x:y:ys) -> (x + 1 <= y && y <= x + 3) && isReportSafeInc (y:ys)
+  _ -> True
+
+part1 :: IO ()
+part1 = do
+  reports <- readReports
+  let
+    isReportSafe :: [Int] -> Bool
+    isReportSafe report = isReportSafeInc report || isReportSafeInc (reverse report)
+  let numSafeReports = length . filter isReportSafe $ reports
+  print numSafeReports
+
+remove1 :: [a] -> [[a]]
+remove1 = \case
+  [x] -> [[]]
+  (x:xs) -> xs : [ x : xs' | xs' <- remove1 xs ]
+
+part2 :: IO ()
+part2 = do
+  reports <- readReports
+  let
+    isReportSafeInc' :: [Int] -> Bool
+    isReportSafeInc' report = isReportSafeInc report || any isReportSafeInc (remove1 report)
+    isReportSafe :: [Int] -> Bool
+    isReportSafe report = isReportSafeInc' report || isReportSafeInc' (reverse report)
+  let numSafeReports = length . filter isReportSafe $ reports
+  print numSafeReports
+
+main :: IO ()
+main = do
+  part1
+  part2
