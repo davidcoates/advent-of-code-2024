cc4d8fea038351214d14b5826b770f2bc6e3c276
David <davidcoates95@gmail.com>
2024-12-03 17:48:28 +1100

    day3 in haskell with no re

---
+++ Day03/main.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE LambdaCase #-}
+
+-- A fun exercise to solve the prolem without regex
+
+import Control.Applicative (Alternative(..))
+
+-- A simple non-backtracking parser with one character of lookahead
+
+newtype Parser a = Parser (String -> (Maybe a, String))
+
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f (Parser p) = Parser $ \ts -> case p ts of
+    (Just a, ts) -> (Just (f a), ts)
+    (Nothing, ts) -> (Nothing, ts)
+
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure x = Parser $ \ts -> (Just x, ts)
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  (<*>) (Parser p1) (Parser p2) = Parser $ \ts -> case p1 ts of
+    (Just f, ts) -> case p2 ts of
+      (Just x, ts) -> (Just (f x), ts)
+      (Nothing, ts) -> (Nothing, ts)
+    (Nothing, ts) -> (Nothing, ts)
+
+instance Alternative Parser where
+  empty :: Parser a
+  empty = Parser $ \ts -> (Nothing, ts)
+  (<|>) :: Parser a -> Parser a -> Parser a
+  (<|>) (Parser p1) (Parser p2) = Parser $ \ts -> case p1 ts of
+    (Just x, ts) -> (Just x, ts)
+    (Nothing, ts) -> p2 ts
+
+match :: (Char -> Bool) -> Parser Char
+match p = Parser $ \ts -> case ts of
+  [] -> (Nothing, [])
+  (x:xs)
+    | p x -> (Just x, xs)
+    | otherwise -> (Nothing, x:xs)
+
+char :: Char -> Parser Char
+char c = match (== c)
+
+string :: String -> Parser String
+string = \case
+  [c] -> (:[]) <$> char c
+  (c:cs) -> (:) <$> char c <*> string cs
+
+between :: Int -> Int -> Parser a -> Parser [a]
+between n m p | m >= n && n >= 0 = between' n m where
+  between' 0 0 = pure []
+  between' 0 m = (:) <$> p <*> between' 0 (m - 1) <|> pure []
+  between' n m = (:) <$> p <*> between' (n - 1) (m - 1)
+
+int :: Parser Int
+int = read <$> (between 1 3 digit) where
+  digit = match (`elem` ['0'..'9'])
+
+mul :: Parser (Int, Int)
+mul = (,) <$> (string "mul(" *> int) <*> (string "," *> int <* string ")")
+
+findAll :: Parser a -> String -> [a]
+findAll (Parser p) = \case
+  [] -> []
+  ts -> case p ts of
+    (Nothing, ts) -> case ts of
+      [] -> []
+      (t:ts) -> findAll (Parser p) ts
+    (Just x, ts) -> x : findAll (Parser p) ts
+
+part1 :: IO ()
+part1 = do
+  ts <- readFile "input.txt"
+  let answer = sum $ findAll ((\(a, b) -> a * b) <$> mul) ts
+  print answer
+
+data Statement = Mul (Int, Int) | Do | Dont
+
+part2 :: IO ()
+part2 = do
+  ts <- readFile "input.txt"
+  let statement = (Mul <$> mul) <|> (string "do" *> ((string "()" *> pure Do) <|> (string "n't()" *> pure Dont))) 
+  let answer = eval True (findAll statement ts)
+  print answer
+  where
+    eval :: Bool -> [Statement] -> Int
+    eval enabled [] = 0
+    eval enabled (x:xs) = case x of
+      Mul (a, b) -> (if enabled then (a * b) else 0) + eval enabled xs
+      Do -> eval True xs
+      Dont -> eval False xs
+
+main :: IO ()
+main = do
+  part1
+  part2
